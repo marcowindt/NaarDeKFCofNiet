1d81ea168b30104256e4789f74bec5a10c57fa2d
Marco Dijkslag <m.dijkslag@student.utwente.nl>
2020-09-28 22:49:37 +0200

    change app UI

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:test_github_actions_flutter/main.dart';
+import 'package:naar_de_kfc_of_niet/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
